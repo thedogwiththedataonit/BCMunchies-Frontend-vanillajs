dcf5f26afe07512a170e83e1cd7b83ef8139467b
Junseo Park <parkaph@bc.edu>
2022-01-07 23:28:32 -0800

    first

---
+++ application.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, request, url_for, jsonify
+import pymongo
+from pymongo import MongoClient
+import certifi
+import re
+from flask_cors import CORS, cross_origin
+
+application = Flask(__name__)
+#CORS(application)
+
+#All this does is serve api requests for data and handle payment processes
+@application.route('/', methods=["POST", "GET"])
+def main():
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    application.run(debug=True)
